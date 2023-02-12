4837583be7ee26ae3f4f747758d8e6811549892f
immemor <>
2023-02-12 05:08:05 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+//for a connection with discord
+const { Client, GatewayIntentBits, messageLink } = require("discord.js");
+const dotenv = require("dotenv");
+// const axios = require("axios"); 
+dotenv.config();
+const keepAlive = require('./server')
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+// console.log(process.env['OPENAI_API'])
+//for a connection with openai api
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  organisation: process.env['OPENAI_ORG'],
+
+  apiKey: process.env.OPENAI_API,
+
+})
+// console.log(configuration)
+// console.log(process.env.OPENAI_API)
+// console.log(process.env.OPENAI_ORG)
+
+
+const openai = new OpenAIApi(configuration);
+
+// console.log(openai)
+// const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+//bot redy test
+client.on("ready", () => {
+  console.log("Bot is ready!");
+});
+
+client.on('messageCreate', async (msg) => {
+  try {
+    if (msg.author.bot) return
+
+    if (msg.content === "!gpt") {
+      msg.reply("I'm Up");
+    }
+    if (msg.content.startsWith("!chat ")) {
+      let text = msg.content.split("!chat ")[1];
+      console.log(text)
+      const gptResponse = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: text,
+        max_tokens: 512,
+        temperature: 0.5,
+        stop: ['ChatGPT:', 'achillies:', 'stopPlease:']
+      })
+      // console.log(gptResponse);
+      msg.reply(`${gptResponse.data.choices[0].text}`);
+      return;
+    }
+  } catch (err) {
+    console.log(err)
+  }
+})
+keepAlive()
+client.login(process.env.TOKEN);
